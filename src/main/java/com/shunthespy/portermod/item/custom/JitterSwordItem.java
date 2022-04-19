39ee48733adbb49733955567626c79eb1a8d01ec
shunthespy <shunthespy@gmail.com>
2022-04-19 15:57:19 -0400

    added weapon damage (no cooldown) and self-hurt when tp

--- src/main/java/com/shunthespy/portermod/item/custom/JitterSwordItem.java
+++ src/main/java/com/shunthespy/portermod/item/custom/JitterSwordItem.java
@@ -1,31 +1,26 @@
 package com.shunthespy.portermod.item.custom;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.NotNull;
-import org.lwjgl.system.CallbackI;
 
 public class JitterSwordItem extends Item {
-    public JitterSwordItem(Properties pProperties) {
-        super(pProperties);
-    }
+    public JitterSwordItem(Properties pProperties) { super(pProperties); }
 
     private static BlockHitResult getPlayerRayResult(Level pLevel, Player pPlayer, double dist) { //grabbed from getPlayerPOVHitResult
         float f = pPlayer.getXRot();
@@ -68,6 +63,14 @@ public class JitterSwordItem extends Item {
 
         pPlayer.level.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0f, 1.0f);
 
+        pPlayer.addEffect(new MobEffectInstance(MobEffects.HARM, 1, 1));
+
         return super.use(pLevel, pPlayer, pUsedHand);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        pTarget.hurt(DamageSource.playerAttack((Player) pAttacker), 8);
+        return false;
     }
 }
